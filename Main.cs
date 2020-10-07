87460c1e97fb1486886898c504779f687209b7bd
unknown <pronixer132sd@gmail.com>
2020-10-07 16:32:50 -0400

    Fixed Hardcoding paths i guess

--- Main.cs
+++ Main.cs
@@ -9,18 +9,20 @@ using System.Threading.Tasks;
 using UnityEngine;
 using System;
 using Exiled.Events.EventArgs;
+using System.Diagnostics;
 //This code is weird haha
-namespace VotingSystem
+namespace DupecExiledUpdater
 {
     public static class DupecUpdater
     {
-
+        private static string pluginPath;
         public static string plugin;
         public static string pluginDLL;
         public static string pluginVersion;
         public static string githubAuthor;
         public static string githubName;
         public static string pastebinID;
+        public static string ppt = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\EXILED\\DupecUpdater\\";
         public static void StartUpdate(string pluginName, string pluginDllName, string pluginVersions, string githubAuthors, string githubRepositoryName, string pastebinId)
         {
             plugin = pluginName;
@@ -29,13 +31,16 @@ namespace VotingSystem
             githubAuthor = githubAuthors;
             githubName = githubRepositoryName;
             pastebinID = pastebinId;
+            pluginPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\EXILED\\DupecUpdater\\{plugin}\\";
+            
             ex2led();
     }
         static void downloadFile()
         {
             Exiled.API.Features.Log.Debug($"Initializing Updater...");
+            File.WriteAllText(pluginPath + "installedVersion.txt", $"{pluginVersion}");
             WebClient webClient = new WebClient();
-            webClient.DownloadFile($"https://pastebin.com/raw/{pastebinID}", $"C:\\{plugin}\\Server\\version.txt");
+            webClient.DownloadFile($"https://pastebin.com/raw/{pastebinID}", pluginPath + "websiteVersion.txt");
             Task.Delay(1000).ContinueWith(t =>
             {
 
@@ -46,10 +51,10 @@ namespace VotingSystem
         static void checkingVersion()
         {
             
-            string clientVersion = File.ReadAllText($"C:\\{plugin}\\Client\\version.txt");
+            string clientVersion = File.ReadAllText(pluginPath + "installedVersion.txt");
             WebClient webClient = new WebClient();
             Exiled.API.Features.Log.Debug($"Checking {plugin} version...");
-            string webVersion = File.ReadAllText($"C:\\{plugin}\\Server\\version.txt");
+            string webVersion = File.ReadAllText(pluginPath + $"websiteVersion.txt");
             
             if (clientVersion == webVersion)
             {
@@ -57,37 +62,27 @@ namespace VotingSystem
             }
             else
             {
-                Exiled.API.Features.Log.Debug($"{plugin} {pluginVersion} is available for download at https://github.com/{githubAuthor}/{githubName}/");
+                Exiled.API.Features.Log.Debug($"{plugin} {webVersion} is available for download at https://github.com/{githubAuthor}/{githubName}/");
             }
         }
         static void ex2led()
         {
-            System.Console.WriteLine($"Checking for Updates...");
-            if (!Directory.Exists($"C:\\{plugin}\\"))
+            if (!Directory.Exists(ppt))
             {
-                System.Console.WriteLine($"Directory {plugin} not found!, Creating...");
-                Directory.CreateDirectory($"C:\\{plugin}\\");
-                Directory.CreateDirectory($"C:\\{plugin}\\dll\\");
-
+                Directory.CreateDirectory(ppt);
+                Exiled.API.Features.Log.Debug($"Directory %appdata%/EXILED/DupecUpdater/ does not exist server is restarting...");
+                Round.Restart();
             }
-            else if (!Directory.Exists($"C:\\{plugin}\\Server\\"))
+            else if(!Directory.Exists(pluginPath))
             {
-                System.Console.WriteLine($"Directory {plugin}/Server/ not found!, Creating...");
-                Directory.CreateDirectory($"C:\\{plugin}\\Server\\");
-                ex2led();
-            }
-            else if (!Directory.Exists($"C:\\{plugin}\\Client\\"))
-            {
-                Exiled.API.Features.Log.Debug($"Directory {plugin}/Client/ not found!, Creating...");
-                Directory.CreateDirectory($"C:\\{plugin}\\Client\\");
-                File.Create($"C:\\{plugin}\\Client\\version.txt");
-
-                File.WriteAllText($"C:\\{plugin}\\Client\\version.txt", $"{pluginVersion}");
-                ex2led();
+                Directory.CreateDirectory(pluginPath);
+                File.Create(pluginPath + "installedVersion.txt");
+                File.Create(pluginPath + "websiteVersion.txt");
+                Exiled.API.Features.Log.Debug($"Directory %appdata%/EXILED/DupecUpdater/{plugin}/ does not exist server is restarting...");
+                Round.Restart();
             }
             else
             {
-                File.WriteAllText($"C:\\{plugin}\\Client\\version.txt", $"{pluginVersion}");
                 downloadFile();
             }
         }
